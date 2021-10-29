8fc83936f3a960b13611cfe2cc368188da82cabd
has <hannes.stefani@outlook.de>
2021-10-29 10:56:46 +0200

    updated envs

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = `http://${process.env.VUE_APP_BACKEND}/api/auth/`;
+const API_URL = `${process.env.VUE_APP_BACKEND}/api/auth/`;
 
 class AuthService {
     login(user) {
--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = `http://${process.env.VUE_APP_BACKEND}/api/test/`;
+const API_URL = `${process.env.VUE_APP_BACKEND}/api/test/`;
 class UserService {
     getPublicContent() {
         return axios.get(API_URL + 'all');
